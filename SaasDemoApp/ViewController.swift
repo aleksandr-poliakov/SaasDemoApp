4976d733d95d2b458b623ee2d384da2acc836d85
alexander.poliakov <alexander.poliakov96@gmail.com>
2022-09-20 13:02:10 +0300

    Initial Commit

---
+++ SaasDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaasDemoApp
+//
+//  Created by Aleksandr on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
